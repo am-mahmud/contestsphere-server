1d02d986446e658891cfa6710d4edf1cef70360e
am-mahmud <am.mahmudasif@gmail.com>
2025-12-08 15:37:43 -0500

    auth system updated

--- routes/auth.js
+++ routes/auth.js
@@ -4,22 +4,18 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-// Register Route
 router.post('/register', async (req, res) => {
   try {
     const { name, email, password, photo } = req.body;
 
-    // Check if user exists
     let user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create new user
     user = new User({
       name,
       email,
@@ -29,7 +25,7 @@ router.post('/register', async (req, res) => {
 
     await user.save();
 
-    // Create JWT token
+    
     const token = jwt.sign(
       { userId: user._id, role: user.role },
       process.env.JWT_SECRET,
