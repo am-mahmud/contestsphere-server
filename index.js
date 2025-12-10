f3730651999862741e146abe9a491a6ba843541e
am-mahmud <am.mahmudasif@gmail.com>
2025-12-10 02:09:36 -0500

    file updated

--- index.js
+++ index.js
@@ -5,25 +5,22 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Connect to database
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contests', require('./routes/contest'));
 app.use('/api/users', require('./routes/user'));
 app.use('/api/participations', require('./routes/participation'));
 
-// Test route
+
 app.get('/', (req, res) => {
   res.json({ message: 'ContestSphere API Running' });
 });
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
